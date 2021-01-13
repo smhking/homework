5d0781159997d22af382ad59345f1205eb97262c
smhking <77413992+smhking@users.noreply.github.com>
2021-01-14 00:08:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace naw_w_taman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; int age; int year = 2021; int result;
+
+            Console.WriteLine("enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("enter your date of birth: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            result = year - age;
+
+            Console.WriteLine("<<<< your name is " + name + " and your age is " + result+" years >>>>");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
